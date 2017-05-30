5c56b6275f6b195c0059041a64061d4e0628e554
Jean Charles Santi <jean.charles.santi@gmail.com>
2017-05-30 21:12:54 +0200

    added 's' at end of 'show-language' file, added forks and stars files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 const actions = {
-  'trending-stars': require('./trending-stars'),
-  'trending-forks': require('./trending-forks'),
-  'show-language': require('./show-languages'),
+  'most-stars': require('./most-stars'),
+  'most-forks': require('./most-forks'),
+  'show-languages': require('./show-languages'),
   'laugh': require('./laugh'),
 }
 
